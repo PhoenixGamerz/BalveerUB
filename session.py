04263cc98e0b3e33e42a34cc3eb9649de6ddba40
Phoenix Gamerz <114356392+PhoenixGamerz@users.noreply.github.com>
2022-10-16 10:18:05 +0530

    Update session.py

--- session.py
+++ session.py
@@ -8,7 +8,7 @@ from pyrogram import Client
 # guide them 
 
 intro = """
-@BalveerUB_Updates Corporation
+@Balveer_Updates Corporation
 Get the following values by logging to,
 
 https://my.telegram.org
